7d27f00d32534952a5016cc236ce0a08cd1708af
Nahum <losond8@gmail.com>
2021-07-08 16:48:42 -0400

    How to create #initialize methods and use them to set attributes when an instance is created.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+
+    #Use initialize to set attributes when an instance is created
+    def initialize(name, breed = "Mutt")
+        #instance variables
+        @name = name
+        @breed = breed
+    end
+
+end
+
